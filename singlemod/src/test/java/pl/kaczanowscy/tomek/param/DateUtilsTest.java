c4094803b15257c4f946126e9c83ecca70556613
Maciej Zasada <maciej.zasada@allegro.pl>
2013-06-17 10:52:55 +0200

    EX-001 | Przepisanie testów na parametryzowane z TestNG.

--- singlemod/src/test/java/pl/kaczanowscy/tomek/param/DateUtilsTest.java
+++ singlemod/src/test/java/pl/kaczanowscy/tomek/param/DateUtilsTest.java
@@ -1,42 +1,38 @@
 package pl.kaczanowscy.tomek.param;
 
-import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
-import static org.testng.Assert.assertEquals;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 @Test
 public class DateUtilsTest {
 
-    public void secondJuneWasSunday() {
-        Date date = createDate(2013, 5, 2);
+  @Test(dataProvider = "testCases")
+  public void shouldCreateValidDate(Date given, Day expected) {
+    Day day = DateUtils.getDayOfWeek(given);
 
-        Day day = DateUtils.getDayOfWeek(date);
+    assertEquals(day, expected);
+  }
 
-        assertEquals(day, Day.Sunday);
-    }
+  private static Date createDate(int year, int month, int day) {
+    Calendar cal = Calendar.getInstance();
+    cal.set(year, month, day);
+    return cal.getTime();
+  }
 
-    public void thirdMayWasFriday() {
-        Date date = createDate(2013, 4, 3);
+  @DataProvider(name = "testCases")
+  public Object[][] prepareTestCaseData() {
+    List<Object[]> testCases = new ArrayList<Object[]>();
+    testCases.add(new Object[] { createDate(2013, 5, 2), Day.Sunday });
+    testCases.add(new Object[] { createDate(2013, 4, 3), Day.Friday });
+    testCases.add(new Object[] { createDate(2013, 3, 10), Day.Wednesday });
 
-        Day day = DateUtils.getDayOfWeek(date);
-
-        assertEquals(day, Day.Friday);
-    }
-
-    public void tenthAprilWasWednesday() {
-        Date date = createDate(2013, 3, 10);
-
-        Day day = DateUtils.getDayOfWeek(date);
-
-        assertEquals(day, Day.Wednesday);
-    }
-
-    private static Date createDate(int year, int month, int day) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(year, month, day);
-        return cal.getTime();
-    }
+    return testCases.toArray(new Object[testCases.size()][]);
+  }
 }
